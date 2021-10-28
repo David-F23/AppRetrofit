769531bab6e4b722b737eb12a0bc7bb1bd69bb77
David-FV <floresdavide0@gmail.com>
2021-10-28 14:45:47 -0600

    Se subio el proyecto

---
+++ app/src/main/java/Model/Posts.java
@@ -0,0 +1,25 @@
+package Model;
+
+public class Posts {
+
+    private int userId;
+    private int id;
+    private String tittle;
+    private String body;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
